9a1b3cb8cd361b59e07cc235192b0f1902005be6
Zoran Kokeza <zoran.kokeza@htecgroup.com>
2024-02-27 10:24:13 +0100

    feat: add dummy error

--- apps/org/src/app/app.tsx
+++ apps/org/src/app/app.tsx
@@ -1,9 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useEffect } from 'react';
 import styles from './app.module.scss';
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      throw new Error('test');
+    }, 1000);
+  }, []);
   return (
     <div>
       <NxWelcome title="org" />
